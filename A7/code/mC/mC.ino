1577fdcc2c59f1f2eb727337fded81c7e006873f
Jacob <jcmelend@asu.edu>
2021-11-24 19:07:23 -0700

    A7

--- A7/code/mC/mC.ino
+++ A7/code/mC/mC.ino
@@ -8,11 +8,11 @@ TTGOClass *ttgo;
 #include <HTTPClient.h>
 
 
-const char* ssid = "Im a virus!";
-const char* password = "feionyy88";
+const char* ssid = "";
+const char* password = "";
 
 //Your Domain name with URL path or IP address with path
-const char* serverName = "http://34.234.66.7:1234/setValue";
+const char* serverName = "http://192.168.0.195:1234/setValue";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -28,7 +28,7 @@ String response;
 //      VCC: 5V or 3V
 //      GND: GND
 //      DATA: 21 or 25
-int pinDHT11 = 25;
+int pinDHT11 = 21;
 SimpleDHT11 dht11(pinDHT11);
 
 
